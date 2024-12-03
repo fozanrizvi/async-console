7390d2ff44d94780484b490c186cbe1794a8c6d9
Fozan Rizvi <fozanrizvi@gmail.com>
2024-12-02 20:15:34 -0600

    Changes implemented in Async Console

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Net.Http;
+
+HttpClient client = new();
+HttpResponseMessage response = await client.GetAsync("https://www.apple.com");  // This will throw an exception
+WriteLine("Apple's home page has {0:N0} bytes.",
+response.Content.Headers.ContentLength);
